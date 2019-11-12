39ebaea655c2fa42a2e134a7e8727d9fac3245bb
Legorobotdude <adityabawankule@gmail.com>
2019-11-12 17:43:57 -0600

    Add horizontal/vertical line drawing

--- Src/gfx.c
+++ Src/gfx.c
@@ -6,6 +6,7 @@
  */
 
 #include "font.h"
+#include "gfx.h"
 
 uint8_t draw_char(uint8_t character, uint16_t pos_x, uint16_t pos_y, uint8_t* fb){
   uint16_t x,y;
@@ -75,5 +76,33 @@ void draw_string_scaled(uint8_t* str, uint16_t n, uint16_t pos_x, uint16_t pos_y
 
   for(i = 0; i < n; i++){
     horiz_pos += draw_char_scaled(str[i], horiz_pos, pos_y, fb, scale);
+  }
+}
+
+void draw_vert_line(uint16_t pos_x, uint16_t pos_y1, uint16_t pos_y2, uint8_t* fb)
+{
+  if (pos_y2 < pos_y1)//swap if needed
+  {
+    uint16_t temp = pos_y1;
+    pos_y1 = pos_y2;
+    pos_y2 = temp;
+  }
+  for (int i = pos_y1; i <=pos_y2; i++)
+  {
+    fb[pos_x + (i) * SCREEN_WIDTH] = 0xFF;
+  }
+}
+
+void draw_hor_line(uint16_t pos_x1, uint16_t pos_x2, uint16_t pos_y, uint8_t* fb)
+{
+  if (pos_x2 < pos_x1)//swap if needed
+  {
+    uint16_t temp = pos_x1;
+    pos x1 = pos_x2;
+    pos_x2 = temp;
+  }
+  for (int i = pos_x1; i <=pos_x2; i++)
+  {
+    fb[i + (pos_y) * SCREEN_WIDTH] = 0xFF;
   }
 }
--- Src/gfx.h
+++ Src/gfx.h
@@ -8,8 +8,13 @@
 #ifndef GFX_H_
 #define GFX_H_
 
+#define SCREEN_WIDTH 480
+
 uint8_t draw_char(uint8_t character, uint16_t pos_x, uint16_t pos_y, uint8_t* fb);
 uint8_t draw_char_scaled(uint8_t character, uint16_t pos_x, uint16_t pos_y, uint8_t* fb, int16_t scale);
 void draw_string(uint8_t* str, uint16_t n, uint16_t pos_x, uint16_t pos_y, uint8_t* fb);
+
+void draw_vert_line(uint16_t pos_x, uint16_t pos_y1, uint16_t pos_y2, uint8_t* fb);
+void draw_hor_line(uint16_t pos_x1, uint16_t pos_x2, uint16_t pos_y, uint8_t* fb);
 
 #endif /* GFX_H_ */
